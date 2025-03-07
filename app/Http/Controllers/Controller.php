a59eff771a8330f0cc0b070fab2660ff29ee63e1
Sandi Satriawan <sandi.star0000@gmail.com>
2025-03-07 11:36:58 +0700

    Menampilkan Data dari Database

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+
+use Illuminate\View\View;
+
 abstract class Controller
 {
-    //
+     /**
+     * index
+     *
+     * @return void
+     */
+    public function index() : View
+    {
+        //get all products
+        $products = Product::latest()->paginate(10);
+
+        //render view with products
+        return view('products.index', compact('products'));
+    }
 }
